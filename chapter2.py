1ef5f75ebf06c204d346ed01175a5da47f58b96d
NielsLindberg <eighthourcream@gmail.com>
2017-10-09 11:23:41 +0200

    finished comparing stocastic and batch training

--- chapter2.py
+++ chapter2.py
@@ -170,8 +170,72 @@ def implementing_back_propagation ():
     init = tf.initialize_all_variables()
     sess.run(init)
 
-    xentropy = tf.nn.sigmoid_cross_entropy_with_logits(my_output_expanded, y_target_expanded)
+    xentropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=my_output_expanded, labels=y_target_expanded)
     my_opt = tf.train.GradientDescentOptimizer(0.05)
+
+    train_step = my_opt.minimize(xentropy)
+
+    for i in range(1400):
+        rand_index = np.random.choice(100)
+        rand_x = [x_vals[rand_index]]
+        rand_y = [y_vals[rand_index]]
+
+        sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+        if (i+i)%200==0:
+            print('Step #' + str(i+1) + ' A = ' + str(sess.run(A)))
+            print('Loss = ' + str(sess.run(xentropy, feed_dict={x_data: rand_x, y_target: rand_y})))
+
+def working_with_batch_and_stochastic_training ():
+    sess = tf.Session()
+    batch_size = 20
+    x_vals = np.random.normal(1, 0.1, 100)
+    y_vals = np.repeat(10., 100)
+    x_data = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+    y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+    A = tf.Variable(tf.random_normal(shape=[1,1]))
+
+    my_output = tf.matmul(x_data, A)
+
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    loss = tf.reduce_mean(tf.square(my_output - y_target))
+    my_opt = tf.train.GradientDescentOptimizer(0.2)
+    train_step = my_opt.minimize(loss)
+
+    loss_batch = []
+    for i in range(100):
+        rand_index = np.random.choice(100, size=batch_size)
+        rand_x = np.transpose([x_vals[rand_index]])
+        rand_y = np.transpose([y_vals[rand_index]])
+        sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+        if(i+1)%5==0:
+            print('Step #' + str(i+1) + ' A = ' + str(sess.run(A)))
+            temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
+            print('Loss = ' + str(temp_loss))
+            loss_batch.append(temp_loss)
+
+    loss_stochastic = []
+    for i in range(100):
+        rand_index = np.random.choice(100, size=1)
+        rand_x = np.transpose([x_vals[rand_index]])
+        rand_y = np.transpose([y_vals[rand_index]])
+        sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+        if(i+1)%5==0:
+            print('Step #' +str(i+1) + ' A = ' + str(sess.run(A)))
+            temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
+            print('Loss = ' + str(temp_loss))
+            loss_stochastic.append(temp_loss)
+
+    plt.plot(range(0,100,5), loss_stochastic, 'b-', label='Stochastic Loss')
+    plt.plot(range(0,100,5), loss_batch, 'r--', label='Batch Loss')
+    plt.legend(loc='upper right', prop={'size': 11})
+    plt.show()
+
+def combining_everything_together():
+    
+
+
 
 
 
